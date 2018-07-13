5332434044d40259c7b0c767288390961a5192d4
lianghui <lianghui_g@lianghui-gdeMacBook-Pro.local>
2018-07-13 17:19:56 +0800

    add zhu shi

--- app/src/main/java/com/example/lianghui/gittest/MainActivity.java
+++ app/src/main/java/com/example/lianghui/gittest/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.lianghui.gittest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 主activity
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
